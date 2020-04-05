fac471972bce41ac28c46e0c5b9653aed3d9d0eb
EvanReinaldo <61820852+EvanReinaldo@users.noreply.github.com>
2020-04-05 14:11:02 +1000

    Update test_me.sh

--- assignment1/test_me.sh
+++ assignment1/test_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then #check if there's and argument, if equal 0 / no argument then echo the NOT
 echo "This is NOT a test"
 else
 echo "This is a TEST"
