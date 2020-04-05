2cbb6163386904e82c17c50ad12beb4f6d0dae94
EvanReinaldo <61820852+EvanReinaldo@users.noreply.github.com>
2020-04-05 14:09:09 +1000

    Update newer.sh

--- assignment1/newer.sh
+++ assignment1/newer.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-result=$1
- for i in $*; do
-   if [ "$i" -nt "$result" ]; then
-    result=$i
+result=$1  #default variable by using the first file for comparing
+ for i in $*; do  #looping for every file that type (if 3 file then the process is looping 3 time)
+   if [ "$i" -nt "$result" ]; then #check if the existing file is newer than the variable $result
+    result=$i #if newer than variable result is replace by the newer file
    fi
  done
-echo $result
+echo $result #display the newest file
