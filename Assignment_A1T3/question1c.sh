dc4912359ee88f86497acab1a2b895c5627a390e
EvanReinaldo <61820852+EvanReinaldo@users.noreply.github.com>
2020-04-15 22:21:46 +1000

    Update question1c.sh

--- Assignment_A1T3/question1c.sh
+++ Assignment_A1T3/question1c.sh
@@ -6,10 +6,10 @@ sort annotation.txt -r > annotation_sorted.txt
 sort expression.txt -r > expression_sorted.txt
 
 #use the join command to join sorted both file
-join expression_sorted.txt annotation_sorted.txt > result.txt
+join expression_sorted.txt annotation_sorted.txt > joinresult.txt
 
 #command to not print the third column, which is metabolism that we don't need
 awk '{$3=""; print $0}' result.txt > finalresult.txt
 
 #reference
-https://unix.stackexchange.com/questions/222121/how-to-remove-a-column-or-multiple-columns-from-file-using-shell-command
+#https://unix.stackexchange.com/questions/222121/how-to-remove-a-column-or-multiple-columns-from-file-using-shell-command
