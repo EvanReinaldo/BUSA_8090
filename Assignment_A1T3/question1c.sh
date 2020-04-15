4a2babbd49ce16ea6fd3173a4b8853353bf2b2b8
EvanReinaldo <61820852+EvanReinaldo@users.noreply.github.com>
2020-04-15 22:23:28 +1000

    Update question1c.sh

--- Assignment_A1T3/question1c.sh
+++ Assignment_A1T3/question1c.sh
@@ -9,7 +9,7 @@ sort expression.txt -r > expression_sorted.txt
 join expression_sorted.txt annotation_sorted.txt > joinresult.txt
 
 #command to not print the third column, which is metabolism that we don't need
-awk '{$3=""; print $0}' result.txt > finalresult.txt
+awk '{$3=""; print $0}' joinresult.txt > finalresult.txt
 
 #reference
 #https://unix.stackexchange.com/questions/222121/how-to-remove-a-column-or-multiple-columns-from-file-using-shell-command
