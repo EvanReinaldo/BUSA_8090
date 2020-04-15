8fb1f04b6e512a802447369d8eb6e381a953019e
EvanReinaldo <61820852+EvanReinaldo@users.noreply.github.com>
2020-04-15 22:07:54 +1000

    Add files via upload

---
+++ Assignment_A1T3/question1c.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sort both file so the gene has the same value
+#i use -r means reverse so the header is still at top
+sort annotation.txt -r > annotation_sorted.txt
+sort expression.txt -r > expression_sorted.txt
+
+#use the join command to join sorted both file
+join expression_sorted.txt annotation_sorted.txt > result.txt
+
+#command to not print the third column, which is metabolism that we don't need
+awk '{$3=""; print $0}' result.txt > finalresult.txt
+
+#reference
+https://unix.stackexchange.com/questions/222121/how-to-remove-a-column-or-multiple-columns-from-file-using-shell-command
